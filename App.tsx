20abf44b628647b8b7fe8d610ae9eb89ca37a907
ThiagoMarques <thiagossmarques@gmail.com>
2023-05-27 18:01:56 -0300

    feat: insere estado para botões da tela inicial

--- App.tsx
+++ App.tsx
@@ -5,10 +5,8 @@ import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
@@ -19,27 +17,40 @@ function App(): JSX.Element {
   state = {
     displayValue: '0'
   }
+
+  addDigit = n => {
+    this.setState({ displayValue: n })
+  }
+
+  clearMemory = () => {
+    this.setState({ displayValue: '0'})
+  }
+
+  setOperation = operation => {
+
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Display value={this.state.displayValue} />
       <View style={styles.buttons}>
-          <Button label="AC" />
-          <Button label="/" />
-          <Button label="7" />
-          <Button label="8" />
-          <Button label="9" />
-          <Button label="*" />
-          <Button label="4" />
-          <Button label="5" />
-          <Button label="6" />
-          <Button label="-" />
-          <Button label="1" />
-          <Button label="2" />
-          <Button label="3" />
-          <Button label="+" />
-          <Button label="0" />
-          <Button label="." />
-          <Button label="=" />
+          <Button label="AC" triple onClick={this.clearMemory} />
+          <Button label="/" operation onClick={() => this.setOperation('/')} />
+          <Button label="7" onClick={this.addDigit} />
+          <Button label="8" onClick={this.addDigit} />
+          <Button label="9" onClick={this.addDigit} />
+          <Button label="*" operation onClick={() => this.setOperation('*')}/>
+          <Button label="4" onClick={this.addDigit}/>
+          <Button label="5" onClick={this.addDigit}/>
+          <Button label="6" onClick={this.addDigit}/>
+          <Button label="-" operation onClick={() => this.setOperation('-')}/>
+          <Button label="1" onClick={this.addDigit}/>
+          <Button label="2" onClick={this.addDigit}/>
+          <Button label="3" onClick={this.addDigit}/>
+          <Button label="+" operation onClick={() => this.setOperation('+')}/>
+          <Button label="0" double onClick={this.addDigit}/>
+          <Button label="." onClick={this.addDigit}/>
+          <Button label="=" operation onClick={() => this.setOperation('=')}/>
         </View>
     </SafeAreaView>
   );
