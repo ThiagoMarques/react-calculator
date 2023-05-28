0c57605964b4492937ed08ac049104624689645f
ThiagoMarques <thiagossmarques@gmail.com>
2023-05-28 07:38:34 -0300

    chore: ajusta renderização de botões no app

--- App.tsx
+++ App.tsx
@@ -2,67 +2,63 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-import Button from './src/components/Button'
-import Display from './src/components/Display'
+import Button from './src/components/Button';
+import Display from './src/components/Display';
 
-function App(): JSX.Element {
+class App extends React.Component {
   state = {
-    displayValue: '0'
-  }
+    displayValue: '0',
+  };
 
   addDigit = n => {
-    this.setState({ displayValue: n })
-  }
+    this.setState({ displayValue: n });
+  };
 
   clearMemory = () => {
-    this.setState({ displayValue: '0'})
-  }
+    this.setState({ displayValue: '0' });
+  };
 
   setOperation = operation => {
+    
+  };
 
-  }
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Display value={this.state.displayValue} />
-      <View style={styles.buttons}>
-          <Button label="AC" triple onClick={this.clearMemory} />
-          <Button label="/" operation onClick={() => this.setOperation('/')} />
-          <Button label="7" onClick={this.addDigit} />
-          <Button label="8" onClick={this.addDigit} />
-          <Button label="9" onClick={this.addDigit} />
-          <Button label="*" operation onClick={() => this.setOperation('*')}/>
-          <Button label="4" onClick={this.addDigit}/>
-          <Button label="5" onClick={this.addDigit}/>
-          <Button label="6" onClick={this.addDigit}/>
-          <Button label="-" operation onClick={() => this.setOperation('-')}/>
-          <Button label="1" onClick={this.addDigit}/>
-          <Button label="2" onClick={this.addDigit}/>
-          <Button label="3" onClick={this.addDigit}/>
-          <Button label="+" operation onClick={() => this.setOperation('+')}/>
-          <Button label="0" double onClick={this.addDigit}/>
-          <Button label="." onClick={this.addDigit}/>
-          <Button label="=" operation onClick={() => this.setOperation('=')}/>
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+       <Button label="AC" triple onClick={this.clearMemory} />
+       <Button label="/" operation onClick={() => this.setOperation('/')} />
+       <Button label="7" onClick={this.addDigit} />
+       <Button label="8" onClick={this.addDigit} />
+       <Button label="9" onClick={this.addDigit} />
+       <Button label="*" operation onClick={() => this.setOperation('*')}/>
+       <Button label="4" onClick={this.addDigit}/>
+       <Button label="5" onClick={this.addDigit}/>
+       <Button label="6" onClick={this.addDigit}/>
+       <Button label="-" operation onClick={() => this.setOperation('-')}/>
+       <Button label="1" onClick={this.addDigit}/>
+       <Button label="2" onClick={this.addDigit}/>
+       <Button label="3" onClick={this.addDigit}/>
+       <Button label="+" operation onClick={() => this.setOperation('+')}/>
+       <Button label="0" double onClick={this.addDigit}/>
+       <Button label="." onClick={this.addDigit}/>
+       <Button label="=" operation onClick={() => this.setOperation('=')}/>
         </View>
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   buttons: {
     flexDirection: 'row',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
   }
 });
 
